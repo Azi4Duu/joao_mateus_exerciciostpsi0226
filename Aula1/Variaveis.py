58c73a1ab4f2fce7d2477f5de90fa0ab1b3bc097
Joao Mateus <jflmateus@gmail.com>
2026-02-27 23:07:11 +0000

    name Aula to Variaveis and created operadores

---
+++ Aula1/Variaveis.py
@@ -0,0 +1,43 @@
+
+'''
+comentar
+varias linhas 
+'''
+#output para a consola
+print("Hello World TPSI0226")
+
+#Int
+tel=9 # decimal e é 9
+print(type(tel))
+print(tel)
+
+#Variaveis dinamicas alteram de tipo automaticamente
+tel="9" # decimal vale 57
+print(type(tel))
+print(tel)
+
+#float 
+med=2.3
+print(type(med))
+print(med)
+
+#string
+nom="Joao"
+print(type(nom))
+print(nom)
+
+#boolean
+flag=True
+print(type(flag))
+print(flag)
+
+#listas []
+lista=[1,"joao",4.4,False,9]
+print(type(lista))
+print(lista)
+
+#tuples () parecidas com listas mas não podem ser alteradas
+tupla=(1,3,4,6)
+print(type(tupla))
+print(tupla)
+
---
+++ Aula1/operadores.py
@@ -0,0 +1,62 @@
+#operadores aritemeticos
+
+# soma +, sub -, div /, mult *, mode %, exponencial **
+
+total=0
+num1=0
+num2=0
+
+#input
+num1=int(input("Insira o 1º numero: "))
+num2=int(input("Insira o 2º numero: "))
+
+total=num1+num2
+print(f"soma: {total}")
+
+total=num1-num2
+print("sub: ", total, " .")
+
+total=num1*num2
+print(f"mult: {total}")
+
+total=num1/num2
+print("div: ", total, " .")
+
+# operadores de controlo de decisão
+
+# == igualdade, != diferente, > maior que, < menor que,
+#  >= maior ou igual, <= menor ou igual
+
+# expressão
+# Val1 == Val2 = True/False
+
+# operadores logicos
+# and e o or
+
+# expressão
+# Val1 > Val2 and Val2 > Val3 = True/False
+#    true     and    false    = False
+
+# If
+
+
+#exercicio encontra o maior e o menor
+val1=2
+val2=3
+val3=4
+
+if val1>val2 and val1>val3:
+    if val2>val3:
+        print("Val1 é o maior e Val3 o menor")
+    else:
+        print("Val1 é o maior e Val2 o menor")
+elif val2>val1 and val2>val3:
+    if val1>val3:
+        print("Val2 é o maior e Val3 o menor")    
+    else:
+        print("Val2 é o maior e Val1 o menor")
+elif val3>val1 and val3>val2:
+    if val1>val2:
+        print("Val3 é o maior e Val2 o menor")    
+    else:
+        print("Val3 é o maior e Val1 o menor")
