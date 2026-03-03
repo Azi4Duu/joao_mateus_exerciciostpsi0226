3518d088a0fe3003914ff24fb46924dd8f7b416e
Joao Mateus <jflmateus@gmail.com>
2026-03-03 17:48:23 +0000

    Added exe2

---
+++ Aula2/Exercicios_if/exe2.py
@@ -0,0 +1,26 @@
+num1=0
+num2=0
+num3=0
+
+num1=int(input("Indique o primeiro nº: "))
+num2=int(input("Indique o segundo nº: "))
+num3=int(input("Indique o terceiro nº: "))
+
+if num1 > num2 and num2 > num3:
+    print(f"Maior: {num1}")
+    print(f"Menor: {num3}")
+elif num1 > num3 and num3 > num2:
+    print(f"Maior: {num1}")
+    print(f"Menor: {num2}")
+elif num2 > num1 and num1 > num3:
+    print(f"Maior: {num2}")
+    print(f"Menor: {num3}")
+elif num2 > num3 and num3 > num1:
+    print(f"Maior: {num2}")
+    print(f"Menor: {num1}")
+elif num3 > num1 and num1 > num2:
+    print(f"Maior: {num3}")
+    print(f"Menor: {num2}")
+elif num3 > num2 and num2 > num1:
+    print(f"Maior: {num3}")
+    print(f"Menor: {num1}")
