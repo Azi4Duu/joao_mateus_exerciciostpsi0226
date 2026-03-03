93b7296d1fb3007d8acb4e74d081af0aec8df779
Joao Mateus <jflmateus@gmail.com>
2026-03-03 18:20:02 +0000

    Patched exe5 and exe2

--- Aula2/Exercicios_if/exe2.py
+++ Aula2/Exercicios_if/exe2.py
@@ -21,6 +21,6 @@ elif num2 > num3 and num3 > num1:
 elif num3 > num1 and num1 > num2:
     print(f"Maior: {num3}")
     print(f"Menor: {num2}")
-elif num3 > num2 and num2 > num1:
+else:
     print(f"Maior: {num3}")
     print(f"Menor: {num1}")
--- Aula2/Exercicios_if/exe5.py
+++ Aula2/Exercicios_if/exe5.py
@@ -21,6 +21,6 @@ elif num2 > num3 and num3 > num1:
 elif num3 > num1 and num1 > num2:
     print(f"Crescente: {num2}, {num1}, {num3}")
     print(f"Decrescente: {num3}, {num1}, {num2}")
-elif num3 > num2 and num2 > num1:
+else:
     print(f"Crescente: {num1}, {num2}, {num3}")
     print(f"Decrescente: {num3}, {num2}, {num1}")
