0fca0ff0807807239c3d6ae27b215e22db965ce4
Joao Mateus <jflmateus@gmail.com>
2026-04-02 23:04:15 +0100

    Added exe1 Dicionarios

---
+++ Aula5/Exercicios_dicionarios/exe1.py
@@ -0,0 +1,41 @@
+import os
+
+alunos = []
+
+def inserir(lista):
+
+    nome = input("Nome: ")
+    idade = input("Idade: ")
+    curso = input("Curso: ")
+
+    aluno = {"Nome" : nome, "Idade" : idade, "Curso" : curso}
+    lista.append(aluno)
+
+
+while True:
+
+    os.system("cls")
+
+    print("MENU")
+    print("1- Inserir")
+    print("2- Listar")
+
+    opt = input("Insira uma opção: ")
+
+    match opt :
+        case "1":
+            inserir(alunos)
+        case "2":
+            os.system("cls")
+            for aluno in alunos:
+                print("-----------------------")
+                print(f"Nome: {aluno["Nome"]}")
+                print(f"Idade: {aluno["Idade"]}")
+                print(f"Curso: {aluno["Curso"]}")
+            input()
+        case "3":
+            break
+        case _:
+            print("Não inseriste uma opção válida!")
+
+
