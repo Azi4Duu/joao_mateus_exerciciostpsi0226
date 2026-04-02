0deaaddc379588f575062726171f0c6e7b09b71a
Joao Mateus <jflmateus@gmail.com>
2026-04-02 22:16:58 +0100

    merge

--- Aula4/dicionarios.py
+++ Aula4/dicionarios.py
@@ -20,27 +20,4 @@ print(carros)
 
 #Remover key
 carros.pop("Ano")
-=======
-# Dicinarios
-# {} <--- dicionarios -> tipo de ficheiro json
-
-carros={"Marca" : "BMW", "Modelo" : "M3"}
-# acesso em Mapping
-# chave - valor
-
-print(carros)
-print(carros["Marca"])
-print(carros["Modelo"])
-
-#Mudar valor de uma key
-carros.update({"Marca" : "Fiat"})
-print(carros)
-
-#Adicionar key
-carros.update({"Ano" : "2000"})
-print(carros)
-
-#Remover key
-carros.pop("Ano")
->>>>>>> ae547a2ce032f2d2508c38f775c151822dc2d86b
 print(carros)
